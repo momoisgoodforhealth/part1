8aa403639d9c942594d71dcf9b1375e75c34ac7f
Shovan Shakya <shoban.shakya@gmail.com>
2021-12-29 16:17:20 -0500

    child components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 
+const App = () => {
+    const [counter, setCounter] = useState(0)
 
-const App = (props) => {
-    const { counter } = props
+    const Display = (props) => {
+        return (
+            <div>{props.counter}</div>
+        )
+    }
+
+    const Button = (props) => {
+        return (
+            <button onClick={props.onClick}>
+                {props.text}
+            </button>
+        )
+    }
+
+    const handleClick = () => {
+        console.log('clicked')
+    }
+
+    const increase1 = () => setCounter(counter + 1)
+    const set0 = () => setCounter(0)
+    const decrease1 = () => setCounter(counter - 1)
+
     return (
-        <div>{counter}</div>
+        <div>
+            <Display counter={counter}/>
+            <Button onClick={increase1}
+                text='plus' />
+            <Button onClick={set0}
+                text='zero' />
+            <Button onClick={decrease1}
+                text='minus' />
+        </div>
     )
 }
-
-
 
 export default App
